28daa485459af9d3ae8b24f6d7cb708bc56610ac
Miguel Xiap <lumigoxi@gmail.com>
2019-08-20 15:13:02 -0600

    first commit safe

---
+++ app/Http/Controllers/memberController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\member;
+use Illuminate\Http\Request;
+
+class memberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $member = new member();
+        $member = $member::all();
+        $response = json_decode($member);
+
+        return view('member.index',[
+            'data'=>$response
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('member.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $response = $request->only('userName', 'name',
+        'lastName', 'password', 'idRole');
+        member::create($response);
+
+        return redirect('miembros');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function show(member $member)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(member $member)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, member $member)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\member  $member
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(member $member)
+    {
+        //
+    }
+}
